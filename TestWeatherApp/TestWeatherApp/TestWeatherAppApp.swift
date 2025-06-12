0827fc40609a12049f49a607bd682f982bbf8c13
Markiian Prysiazhniuk <pryssiaznuk12@gmail.com>
2025-06-12 23:12:12 +0200

    initial commit

---
+++ TestWeatherApp/TestWeatherApp/TestWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestWeatherAppApp.swift
+//  TestWeatherApp
+//
+//  Created by Markiyan Prysiazhniuk on 12.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct TestWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
